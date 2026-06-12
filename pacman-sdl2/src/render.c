edc68160e443deaaf0b65d5a2905dbe288f9becc
Alfredo. C <alfredo.carbajal.sierra@gmail.com>
2026-06-11 20:05:53 -0600

    Update render.c

--- pacman-sdl2/src/render.c
+++ pacman-sdl2/src/render.c
@@ -1,7 +1,6 @@
 #include "game.h"
 #include <stdio.h>
 
-// 1. Inicializar la ventana de SDL2 y cargar la imagen de los personajes
 bool initGame(Game *game) {
     if (SDL_Init(SDL_INIT_VIDEO) < 0) return false;
     if (!(IMG_Init(IMG_INIT_PNG) & IMG_INIT_PNG)) return false;
@@ -14,7 +13,7 @@ bool initGame(Game *game) {
     game->renderer = SDL_CreateRenderer(game->window, -1, SDL_RENDERER_ACCELERATED);
     if (!game->renderer) return false;
 
-    // Carga la hoja de sprites que subiste (debe estar en assets/pacman_sprites.png)
+
     game->spritesheet = IMG_LoadTexture(game->renderer, "assets/pacman_sprites.png");
     if (!game->spritesheet) {
         printf("Error: No se encontro assets/pacman_sprites.png\n");
@@ -26,7 +25,7 @@ bool initGame(Game *game) {
     return true;
 }
 
-// 2. Escuchar el teclado para mover a Pac-Man
+
 void handleEvents(Game *game, Player *player) {
     SDL_Event event;
     while (SDL_PollEvent(&event)) {
@@ -47,33 +46,30 @@ void handleEvents(Game *game, Player *player) {
     }
 }
 
-// 3. Dibujar TODO en la pantalla (Lienzo, laberinto, Pacman y Fantasmas)
 void renderEverything(Game *game, Player *player, Ghost fantasmas[]) {
-    // Fondo negro
+    
     SDL_SetRenderDrawColor(game->renderer, 0, 0, 0, 255);
     SDL_RenderClear(game->renderer);
 
-    // Dibujar el laberinto desde la matriz
     for (int i = 0; i < FILAS; i++) {
         for (int j = 0; j < COLUMNAS; j++) {
             SDL_Rect tile = { j * TILE_SIZE, i * TILE_SIZE, TILE_SIZE, TILE_SIZE };
             
             if (game->mapa[i][j] == '#') {
-                SDL_SetRenderDrawColor(game->renderer, 0, 0, 255, 255); // Paredes Azules
+                SDL_SetRenderDrawColor(game->renderer, 0, 0, 255, 255); 
                 SDL_RenderFillRect(game->renderer, &tile);
             } else if (game->mapa[i][j] == '.') {
-                SDL_SetRenderDrawColor(game->renderer, 255, 184, 174, 255); // Puntos Rosas originales
+                SDL_SetRenderDrawColor(game->renderer, 255, 184, 174, 255); 
                 SDL_Rect pellet = { (j * TILE_SIZE) + 14, (i * TILE_SIZE) + 14, 4, 4 };
                 SDL_RenderFillRect(game->renderer, &pellet);
             }
         }
     }
 
-    // Dibujar a Pac-Man recortando su sprite correspondiente
+
     SDL_Rect player_dst = { player->x * TILE_SIZE, player->y * TILE_SIZE, TILE_SIZE, TILE_SIZE };
     SDL_RenderCopy(game->renderer, game->spritesheet, &player->sprite_rects[player->direction], &player_dst);
 
-    // Dibujar los Fantasmas (Solo si ya fueron liberados por el tiempo de tu compañero)
     for (int i = 0; i < MAXGHOSTS; i++) {
         if (fantasmas[i].is_active) {
             SDL_Rect ghost_dst = { fantasmas[i].x * TILE_SIZE, fantasmas[i].y * TILE_SIZE, TILE_SIZE, TILE_SIZE };
@@ -84,7 +80,6 @@ void renderEverything(Game *game, Player *player, Ghost fantasmas[]) {
     SDL_RenderPresent(game->renderer);
 }
 
-// 4. Cerrar la ventana limpiamente para no dejar trabada la PC
 void cleanSDL(Game *game) {
     if (game->spritesheet) SDL_DestroyTexture(game->spritesheet);
     SDL_DestroyRenderer(game->renderer);
